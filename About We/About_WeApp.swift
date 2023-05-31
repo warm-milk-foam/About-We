0d3ded456ba495171baab36067da812af18b1d8c
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-05-31 13:44:12 +0800

    Initial Commit

---
+++ About We/About_WeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_WeApp.swift
+//  About We
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct About_WeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About We/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  About We
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
